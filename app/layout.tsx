174f727013a019400a5ef58d689412529cca4f02
shahbaz28 <shahbaz.khan2865@gmail.com>
2024-08-26 16:02:11 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-        </ThemeProvider>
+         </ThemeProvider>
       </body>
     </html>
   );
